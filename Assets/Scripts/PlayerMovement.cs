a17803a43c66662e496eefe052567f1934b85de5
Halil İbrahim Kavalcı <110342285+IbrahimKvlci@users.noreply.github.com>
2023-10-06 20:39:23 +0300

    Character Movement & Animations

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D _rb;
+    Animator _animator;
+
+    Vector2 _velocity;
+
+    void Start()
+    {
+        _rb=GetComponent<Rigidbody2D>();
+        _animator=GetComponent<Animator>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        KeyboardControl();
+    }
+
+    void KeyboardControl()
+    {
+        float movementInput = Input.GetAxisRaw("Horizontal");
+        Vector2 scale = transform.localScale;
+
+        if (movementInput > 0)
+        {
+            _velocity.x = Mathf.MoveTowards(_velocity.x, movementInput * 3.0f, 10.0f * Time.deltaTime);
+            _animator.SetBool("Walk", true);
+            scale.x = 1;
+        }
+        else if (movementInput < 0)
+        {
+            _velocity.x = Mathf.MoveTowards(_velocity.x, movementInput * 3.0f, 10.0f * Time.deltaTime);
+            _animator.SetBool("Walk", true);
+            scale.x = -1;
+        }
+        else
+        {
+            _velocity.x = Mathf.MoveTowards(_velocity.x, 0, 50);
+            _animator.SetBool("Walk", false);
+        }
+
+        transform.localScale = scale;
+        transform.Translate(_velocity * Time.deltaTime);
+    }
+}
